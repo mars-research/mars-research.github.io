bacb7ff32df59e5cfbf51b0614d16e79142f1a5f
Zhaofeng Li <hello@zhaofeng.li>
2023-03-20 23:28:14 -0600

    Use Nix to pin the theme

--- flake.nix
+++ flake.nix
@@ -3,27 +3,28 @@
 
   inputs = {
     mars-std.url = "github:mars-research/mars-std";
+    papermod = {
+      url = "github:adityatelange/hugo-PaperMod";
+      flake = false;
+    };
   };
 
-  outputs = { self, mars-std, ... }: let
+  outputs = { self, mars-std, papermod, ... }: let
     supportedSystems = mars-std.lib.defaultSystems;
   in mars-std.lib.eachSystem supportedSystems (system: let
     pkgs = mars-std.legacyPackages.${system};
 
     src = pkgs.nix-gitignore.gitignoreSource [] ./.;
-    vendorSha256 = "sha256-uEzihbpSv5iHwwoNYA9s9o/fRk1MwA45cNxthYj2ASc=";
 
     website = pkgs.stdenv.mkDerivation {
       name = "mars-research-homepage";
       inherit src;
 
-      GIT_SSL_CAINFO = "${pkgs.cacert}/etc/ssl/certs/ca-certificates.crt";
+      nativeBuildInputs = with pkgs; [ hugo ];
 
-      NIX_SSL_CERT_FILE="/etc/ssl/certs/ca-certificates.crt";
-      nativeBuildInputs = with pkgs; [ go hugo openssl_1_1 ];
+      HUGO_MODULE_IMPORTS_PATH = papermod;
 
       buildPhase = ''
-        ln -sf ${hugoVendor} _vendor
         hugo
       '';
 
@@ -33,35 +34,10 @@
 
       dontFixup = true;
     };
-
-    hugoVendor = pkgs.stdenv.mkDerivation {
-      name = "mars-research-homepage-vendor";
-
-      inherit src;
-
-      nativeBuildInputs = with pkgs; [ go hugo
-      (git.override {openssl = openssl_1_1;}) ];
-
-      GIT_SSL_CAINFO = "${pkgs.cacert}/etc/ssl/certs/ca-certificates.crt";
-
-      NIX_SSL_CERT_FILE="/etc/ssl/certs/ca-certificates.crt";
-
-      buildPhase = ''
-        rm -rf /build/hugo_cache
-        hugo mod vendor
-      '';
-
-      installPhase = ''
-        cp -r _vendor $out
-      '';
-
-      outputHashMode = "recursive";
-      outputHashAlgo = "sha256";
-      outputHash = vendorSha256;
-    };
   in {
     devShell = pkgs.mkShell {
       inputsFrom = [ website ];
+      HUGO_MODULE_IMPORTS_PATH = papermod;
     };
     packages.website = website;
     defaultPackage = website;
